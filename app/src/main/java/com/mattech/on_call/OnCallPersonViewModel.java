cbd16d0e0b1c03b8784b1af24fe14d8f05d9f0e8
Mateusz <226069@student.pwr.edu.pl>
2019-09-20 22:29:09 +0200

    Update data added to the ViewModel

--- app/src/main/java/com/mattech/on_call/OnCallPersonViewModel.java
+++ app/src/main/java/com/mattech/on_call/OnCallPersonViewModel.java
@@ -6,19 +6,32 @@ import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
 
 import com.mattech.on_call.models.OnCallPerson;
+import com.mattech.on_call.models.Update;
+
+import java.util.List;
 
 public class OnCallPersonViewModel extends AndroidViewModel {
     private LiveData<OnCallPerson> onCallPerson;
+    private LiveData<List<Update>> updates;
     private OnCallRepository onCallRepository;
 
     public OnCallPersonViewModel(@NonNull Application application) {
         super(application);
         onCallRepository = new OnCallRepository(application);
         onCallPerson = onCallRepository.getOnCallPerson();
+        updates = onCallRepository.getUpdates();
     }
 
     public LiveData<OnCallPerson> getOnCallPerson() {
         return onCallPerson;
+    }
+
+    public LiveData<List<Update>> getUpdates() {
+        return updates;
+    }
+
+    public void addUpdate(Update update) {
+        onCallRepository.addUpdate(update);
     }
 
     public void updateOnCallPerson() {
