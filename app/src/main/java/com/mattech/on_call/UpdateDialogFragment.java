14e388f4c74277abf940d89eb728babb3193c3a0
Mateusz <226069@student.pwr.edu.pl>
2019-09-21 23:49:17 +0200

    All UI elements added to update dialog window and some of them made responsive. State is saved for number pickers and exact date switch.

--- app/src/main/java/com/mattech/on_call/UpdateDialogFragment.java
+++ app/src/main/java/com/mattech/on_call/UpdateDialogFragment.java
@@ -8,6 +8,9 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,9 +18,19 @@ import butterknife.ButterKnife;
 
 public class UpdateDialogFragment extends DialogFragment {
     private OnFragmentInteractionListener listener;
+    private boolean displayDays = true;
+    private final String DISPLAY_DAYS_TAG = "displayDays";
+    private final String HOUR_TAG = "hour";
+    private final String MINUTE_TAG = "minute";
 
-    @BindView(R.id.update_time)
-    TextView time;
+    @BindView(R.id.hour_picker)
+    NumberPicker hour;
+
+    @BindView(R.id.minute_picker)
+    NumberPicker minute;
+
+    @BindView(R.id.days_container)
+    LinearLayout days;
 
     @BindView(R.id.monday)
     TextView monday;
@@ -40,6 +53,12 @@ public class UpdateDialogFragment extends DialogFragment {
     @BindView(R.id.sunday)
     TextView sunday;
 
+    @BindView(R.id.exact_date)
+    TextView exactDate;
+
+    @BindView(R.id.update_type_switch)
+    ImageView updateTypeSwitch;
+
     @BindView(R.id.cancel_btn)
     Button cancelBtn;
 
@@ -56,6 +75,35 @@ public class UpdateDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_update, null);
         ButterKnife.bind(this, view);
+        hour.setMinValue(0);
+        hour.setMaxValue(23);
+        minute.setMinValue(0);
+        minute.setMaxValue(59);
+        if (savedInstanceState != null) {
+            displayDays = savedInstanceState.getBoolean(DISPLAY_DAYS_TAG);
+            hour.setValue(savedInstanceState.getInt(HOUR_TAG));
+            minute.setValue(savedInstanceState.getInt(MINUTE_TAG));
+        } else {
+            hour.setValue(12);
+            minute.setValue(0);
+        }
+        if (!displayDays) {
+            days.setVisibility(View.GONE);
+            exactDate.setVisibility(View.VISIBLE);
+            updateTypeSwitch.setImageDrawable(getResources().getDrawable(R.drawable.repeat, null));
+        }
+        updateTypeSwitch.setOnClickListener(v -> {
+            if (displayDays) {
+                days.setVisibility(View.GONE);
+                exactDate.setVisibility(View.VISIBLE);
+                updateTypeSwitch.setImageDrawable(getResources().getDrawable(R.drawable.repeat, null));
+            } else {
+                exactDate.setVisibility(View.GONE);
+                days.setVisibility(View.VISIBLE);
+                updateTypeSwitch.setImageDrawable(getResources().getDrawable(R.drawable.calendar, null));
+            }
+            displayDays = !displayDays;
+        });
         okBtn.setOnClickListener(v -> {
             if (listener != null) {
                 listener.onOkClick();
@@ -82,5 +130,13 @@ public class UpdateDialogFragment extends DialogFragment {
     public void onDetach() {
         super.onDetach();
         listener = null;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(DISPLAY_DAYS_TAG, displayDays);
+        outState.putInt(HOUR_TAG, hour.getValue());
+        outState.putInt(MINUTE_TAG, minute.getValue());
     }
 }
