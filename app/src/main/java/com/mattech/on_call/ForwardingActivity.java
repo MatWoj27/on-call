9484de06ca9a839ff1faeb554e766e6193cd432e
Mateusz <226069@student.pwr.edu.pl>
2019-10-26 19:44:28 +0200

    Simple notification shown once the call forwarding succeeds or fails due to missing phone number

--- app/src/main/java/com/mattech/on_call/ForwardingActivity.java
+++ app/src/main/java/com/mattech/on_call/ForwardingActivity.java
@@ -1,6 +1,9 @@
 package com.mattech.on_call;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -21,6 +24,21 @@ public class ForwardingActivity extends AppCompatActivity {
     private final String CURRENT_PHONE_NUMBER_TAG = "phoneNum";
     private final String IS_CURRENT_PHONE_NUMBER_SET_TAG = "isPhoneNumSet";
 
+    private enum ForwardingResultState {
+        FORWARDING_SUCCESS(R.string.forwarding_success_title, R.string.forwarding_success_text, R.drawable.success_icon),
+        FORWARDING_FAILURE_NO_REACTOR(R.string.forwarding_failure_title, R.string.forwarding_no_reactor_text, R.drawable.failure_icon);
+
+        int titleId;
+        int textId;
+        int iconId;
+
+        ForwardingResultState(int titleId, int textId, int iconId) {
+            this.titleId = titleId;
+            this.textId = textId;
+            this.iconId = iconId;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +50,7 @@ public class ForwardingActivity extends AppCompatActivity {
         }
         repository.getOnCallPerson().observe(this, onCallPerson -> {
             if (isCurrentPhoneNumberSet) {
+                currentPhoneNumber = onCallPerson.getPhoneNumber();
                 startForwarding(onCallPerson);
             } else {
                 if (onCallPerson != null) {
@@ -65,6 +84,7 @@ public class ForwardingActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        showNotification(ForwardingResultState.FORWARDING_SUCCESS);
         finish();
     }
 
@@ -80,6 +100,22 @@ public class ForwardingActivity extends AppCompatActivity {
                 callForwardingIntent.setData(gsmCode);
                 startActivityForResult(callForwardingIntent, SET_FORWARDING_REQUEST);
             }
+        } else {
+            showNotification(ForwardingResultState.FORWARDING_FAILURE_NO_REACTOR);
         }
+    }
+
+    private void showNotification(ForwardingResultState state) {
+        String description = getResources().getString(state.textId);
+        if (state == ForwardingResultState.FORWARDING_SUCCESS) {
+            description = description + currentPhoneNumber;
+        }
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getResources().getString(state.titleId))
+                .setContentText(description)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
