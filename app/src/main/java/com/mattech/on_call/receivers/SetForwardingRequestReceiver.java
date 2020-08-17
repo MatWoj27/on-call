08436d5352022a935a1461d9c67f6142ebdf561f
Mateusz <226069@student.pwr.edu.pl>
2020-08-17 23:59:51 +0200

    Repeating updates are handled in a separate method to reduce the onReceive() method size

--- app/src/main/java/com/mattech/on_call/receivers/SetForwardingRequestReceiver.java
+++ app/src/main/java/com/mattech/on_call/receivers/SetForwardingRequestReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 
 import com.mattech.on_call.activities.ForwardingActivity;
 import com.mattech.on_call.models.Update;
@@ -17,27 +18,31 @@ public class SetForwardingRequestReceiver extends BroadcastReceiver {
     public static final String EXTRA_UPDATE_TARGET_TIME = "android.intent.extra.ALARM_TARGET_TIME";
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, @NonNull Intent intent) {
         Intent forwardIntent = new Intent(context, ForwardingActivity.class);
-        int updateId = intent.getIntExtra(EXTRA_UPDATE_ID, 0);
         if (!intent.getBooleanExtra(EXTRA_IS_ONE_TIME_UPDATE, false)) {
-            boolean[] repetitionDays = intent.getBooleanArrayExtra(EXTRA_REPETITION_DAYS_TAG);
-            long updateTargetTime = intent.getLongExtra(EXTRA_UPDATE_TARGET_TIME, 0);
-            long nextUpdateTargetTime = Update.getNextRepetitionInMillis(updateTargetTime, repetitionDays);
-            Intent nextUpdateIntent = new Intent(context, SetForwardingRequestReceiver.class);
-            nextUpdateIntent.putExtra(EXTRA_UPDATE_ID, updateId);
-            nextUpdateIntent.putExtra(EXTRA_REPETITION_DAYS_TAG, repetitionDays);
-            nextUpdateIntent.putExtra(EXTRA_IS_ONE_TIME_UPDATE, false);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, updateId, nextUpdateIntent, 0);
-            AlarmManager alarmManager = context.getSystemService(AlarmManager.class);
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, nextUpdateTargetTime, pendingIntent);
+            handleRepeatingUpdate(context, intent);
         } else {
-            forwardIntent.putExtra(ForwardingActivity.EXTRA_DISABLE_UPDATE_ID, updateId);
+            forwardIntent.putExtra(ForwardingActivity.EXTRA_DISABLE_UPDATE_ID, intent.getIntExtra(EXTRA_UPDATE_ID, 0));
         }
         forwardIntent.putExtra(ForwardingActivity.ACTION_TAG, ForwardingActivity.UPDATE_REACTOR_AND_START_FORWARDING_REQUEST_CODE);
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N || Build.VERSION.SDK_INT > Build.VERSION_CODES.O_MR1) {
             forwardIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
         context.startActivity(forwardIntent);
+    }
+
+    private void handleRepeatingUpdate(@NonNull Context context, @NonNull Intent intent) {
+        int updateId = intent.getIntExtra(EXTRA_UPDATE_ID, 0);
+        boolean[] repetitionDays = intent.getBooleanArrayExtra(EXTRA_REPETITION_DAYS_TAG);
+        long updateTargetTime = intent.getLongExtra(EXTRA_UPDATE_TARGET_TIME, 0);
+        long nextUpdateTargetTime = Update.getNextRepetitionInMillis(updateTargetTime, repetitionDays);
+        Intent nextUpdateIntent = new Intent(context, SetForwardingRequestReceiver.class);
+        nextUpdateIntent.putExtra(EXTRA_UPDATE_ID, updateId);
+        nextUpdateIntent.putExtra(EXTRA_REPETITION_DAYS_TAG, repetitionDays);
+        nextUpdateIntent.putExtra(EXTRA_IS_ONE_TIME_UPDATE, false);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, updateId, nextUpdateIntent, 0);
+        AlarmManager alarmManager = context.getSystemService(AlarmManager.class);
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, nextUpdateTargetTime, pendingIntent);
     }
 }
