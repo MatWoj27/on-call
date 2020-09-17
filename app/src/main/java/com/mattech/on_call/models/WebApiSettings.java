479e21129a784efb7ce0215257fbb20ea85ea824
Mateusz <226069@student.pwr.edu.pl>
2020-09-17 23:43:59 +0200

    Model of the web API settings created

---
+++ app/src/main/java/com/mattech/on_call/models/WebApiSettings.java
@@ -0,0 +1,38 @@
+package com.mattech.on_call.models;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+
+import com.mattech.on_call.Constants;
+
+public class WebApiSettings {
+    private String ip;
+    private String port;
+    private String team;
+
+    private WebApiSettings() {
+    }
+
+    public static @NonNull
+    WebApiSettings getInstance(@NonNull Context context) {
+        WebApiSettings result = new WebApiSettings();
+        SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.WEB_API_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        result.ip = sharedPreferences.getString(Constants.WEB_API_IP_PREFERENCE_KEY, "");
+        result.port = sharedPreferences.getString(Constants.WEB_API_PORT_PREFERENCE_KEY, "");
+        result.team = sharedPreferences.getString(Constants.WEB_API_TEAM_PREFERENCE_KEY, "");
+        return result;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+}
