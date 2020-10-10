9d9787adb4585b68fa996cc0c001c615ac471d34
Mateusz <226069@student.pwr.edu.pl>
2020-10-10 23:24:36 +0200

    Hotfix - settings not always displayed when opened from notification

--- app/src/main/java/com/mattech/on_call/activities/MainActivity.java
+++ app/src/main/java/com/mattech/on_call/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mattech.on_call.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -14,7 +15,12 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (getIntent().getIntExtra(ForwardingActivity.ACTION_TAG, 0) == ForwardingActivity.GO_TO_SETTINGS_REQUEST_CODE) {
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getIntExtra(ForwardingActivity.ACTION_TAG, 0) == ForwardingActivity.GO_TO_SETTINGS_REQUEST_CODE) {
             NotificationUtil.cancelActiveForwardingResultNotification(this, Constants.FORWARDING_NOTIFICATION_ID);
             new SettingsDialogFragment().show(getSupportFragmentManager(), "settings");
         }
